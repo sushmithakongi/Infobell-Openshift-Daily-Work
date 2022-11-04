f1f96e13d64f049db5ad09bc5933bf1cedfcae25
meer <meer8586@gmail.com>
2022-11-04 14:25:30 +0530

    msg

---
+++ GitDemo/Swap.java
@@ -0,0 +1,11 @@
+class Swap{
+    public static void main(String[] args){
+        int a=10;
+        int b=20;
+        int temp;
+        temp = a;
+        a = b;
+        b = temp;
+        System.out.println("a : "+a+" b :"+b);
+    }
+}
