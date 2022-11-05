752f9769dbcfa7b1792758d464ac61ed1bef7ae3
Vishal Rajendra Khetmalis <vishalkhetmalis.dbdamarch22@gmail.com>
2022-11-05 20:05:09 +0530

    Adding Swap Variables.py file

---
+++ Swap Variables.py
@@ -0,0 +1,12 @@
+# Creating variables
+num1 = 10
+num2 = 25
+
+# Printing variables
+print("Variables before swapping are num1 : {} & num2 : {}".format(num1,num2))
+
+# Swapping variables
+num1, num2 = num2, num1
+
+# Printing variables
+print("Variables after swapping are num1 : {} & num2 : {}".format(num1,num2))
