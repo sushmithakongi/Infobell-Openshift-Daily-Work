b52f7a5814709b7e905e1f50f4af0cd46172a7ef
Gokulspattom <gokulspattom@gmail.com>
2022-11-07 15:39:56 +0530

    Added swap pgm

---
+++ swap_test.java
@@ -0,0 +1,10 @@
+public class swap_test {
+    public static void main(String[] args) {
+        int a =10,b=20;
+        System.out.println("a is "+a+" b is "+b);
+        int temp = a;
+        a =b;
+        b = temp;
+        System.out.println("a is "+a+" b is "+b);   
+    }
+}
